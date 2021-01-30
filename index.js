5e80f2e13dd53b5a06b509d0de115866336aec39
Abdulrahman Ghalayini <ghalayini.abdulrahman.17@gmail.com>
2021-01-30 00:07:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+
+function findMatching(s ,a){
+
+
+  return s.filter(item => item.toLowerCase() === a.toLowerCase());
+}
+
+
+function fuzzyMatch(s ,a){
+
+
+  return s.filter(item => item.toLowerCase().startsWith(a.toLowerCase()));
+}
+
+
+function matchName(s ,a){
+
+
+  return s.filter(item => item.name.toLowerCase() === a.toLowerCase());
+}
+
+
+
